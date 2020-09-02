fcadb28b2dc44ea02596432eef7bab8a193a6aea
Anton Rott <arott92@gmail.com>
2020-09-02 21:52:40 +0200

    pension models and service stubs added

---
+++ src/app/models/pension-request.spec.ts
@@ -0,0 +1,7 @@
+import { PensionRequest } from './pension-request';
+
+describe('PensionRequest', () => {
+  it('should create an instance', () => {
+    expect(new PensionRequest()).toBeTruthy();
+  });
+});
