27aed33ecf70af5554ebc0d158a68e3f8ce8da65
Jonas_Knebel <jonas.knebel@gmx.de>
2020-09-17 17:20:07 +0200

    Added Model Form and input forms

--- src/app/models/form/form.component.ts
+++ src/app/models/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Form, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { FormModel } from './form-Model';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -9,87 +10,109 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class FormComponent implements OnInit {
   form: FormGroup;
+  formModel: FormModel;
+  formData: FormData;
   url: string = "http://test-api-impl-spring.herokuapp.com/v2/RentenKalkulation";
 
 
-  constructor(public fb: FormBuilder,  private http: HttpClient) {
-    let today = new Date();
-    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    this.form = this.fb.group({
-      artderVersicherung: [''],
-      datumderStandmitteilung: [date],
-      versicherungsbeginn:[],
-      datumRentenbeginn: [],
-      aktuelleBeitragszahlungen: [0],
-      aktuellesVorsorgevermögen: [0],
-      garantiezins: [0],
-      ausgewiesenederzeitigenÜberschussbeteiligung: [0],
-      derzeitigeGesamtverzinsung: [0],
-      höhedergarantiertenAltersrente: [0],
-      höhederAltersrentebeiderzeitigenÜberschussbeteiligungen: [0],
-      höhedergarantiertenAltersrentebeiFortzahlungderderzeitigenBeiträge: [0],
-      höhederAltersrentebeiderzeitigenÜberschussbeteiligungenbeiFortzahlungderderzeitigenBeiträge: [0],
-      fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginn: [0],
-      fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginnbeiFortzahlungderderzeitigenBeiträge: [0],
-      hochrechnungdesgarantiertenAltersvorsorgevermögens: [0],
-      hochrechnungdesAltergarantierterRentenfaktorjeZehnTausendEuroVorsorgeguthabensvorsorgevermögens:[0],
-      hochrechnungdesAltersvorsorgevermögens: [0],
-      rentenfaktor: [0],
-      aktuellerRentenfaktorjeZehnTausendEuroVorsorgeguthaben: [0],
-      garantierterRentenfaktorjeZehnTausendEuroVorsorgeguthaben: [0],
-      aktuellejährlichgezahlteZulagen: [0],
-      grundzulagen: false,
-      anzahlderKinderdieberücksichtigtwerden:[0],
-      zulagen: {
-        grundzulagen: false,
-        Kinderzulagen: {anzahlderKinderdieberücksichtigtwerden: [0]}
-      },
-      anzahlderKinder:[0],
-      geburtsDatumDerKinder:[],
-      kinder: {
-        anzahlderKinder:[0],
-        geburtsDatumDerKinder:[]
-      }
-    })
+  constructor(public fb: FormBuilder, private http: HttpClient) {
+    this.formModel = new FormModel();
+    this.formData = new FormData();
+    this.form = new FormGroup(
+    ({
+      artderVersicherung: new FormControl(),
+      datumderStandmitteilung: new FormControl(),
+      versicherungsbeginn: new FormControl(),
+      datumRentenbeginn: new FormControl(),
+      aktuelleBeitragszahlungen: new FormControl(),
+      aktuellesVorsorgevermögen: new FormControl(),
+      garantiezins: new FormControl(),
+      ausgewiesenederzeitigenÜberschussbeteiligung: new FormControl(),
+      derzeitigeGesamtverzinsung: new FormControl(),
+      höhedergarantiertenAltersrente: new FormControl(),
+      höhederAltersrentebeiderzeitigenÜberschussbeteiligungen: new FormControl(),
+      höhedergarantiertenAltersrentebeiFortzahlungderderzeitigenBeiträge: new FormControl(),
+      höhederAltersrentebeiderzeitigenÜberschussbeteiligungenbeiFortzahlungderderzeitigenBeiträge: new FormControl(),
+      fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginn: new FormControl(),
+      fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginnbeiFortzahlungderderzeitigenBeiträge: new FormControl(),
+      hochrechnungdesgarantiertenAltersvorsorgevermögens: new FormControl(),
+      hochrechnungdesAltersvorsorgevermögens: new FormControl(),
+      rentenfaktor : new FormControl(),
+      aktuellerRentenfaktorjeZehnTausendEuroVorsorgeguthaben: new FormControl(),
+      garantierterRentenfaktorjeZehnTausendEuroVorsorgeguthaben: new FormControl(),
+      aktuellejährlichgezahlteZulagen: new FormControl(),
+      zulagen: new FormControl(),
+      grundzulagen: new FormControl(),
+      kinderzulagen: new FormControl(),
+      anzahlderKinderdieberücksichtigtwerden: new FormControl(),
+      kinder:new FormControl(),
+      anzahlderKinder: new FormControl(),
+      geburtsDatumDerKinder: new FormControl(),
+
+
+    }));
+    this.formModel.artderVersicherung = "";
+    this.formModel.datumderStandmitteilung = null;
+    this.formModel.datumRentenbeginn = null;
+    this.formModel.aktuelleBeitragszahlungen = null;
+    this.formModel.aktuellesVorsorgevermögen = null;
+    this.formModel.garantiezins = null;
+    this.formModel.ausgewiesenederzeitigenÜberschussbeteiligung = null;
+    this.formModel.derzeitigeGesamtverzinsung = null;
+    this.formModel.höhedergarantiertenAltersrente = null;
+    this.formModel.höhederAltersrentebeiderzeitigenÜberschussbeteiligungen = null;
+    this.formModel.höhedergarantiertenAltersrentebeiFortzahlungderderzeitigenBeiträge = null;
+    this.formModel.fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginn = null;
+    this.formModel.fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginnbeiFortzahlungderderzeitigenBeiträge = null;
+    this.formModel.hochrechnungdesgarantiertenAltersvorsorgevermögens = null;
+    this.formModel.hochrechnungdesAltersvorsorgevermögens = null;
+    this.formModel.rentenfaktor = null;
+    this.formModel.aktuellerRentenfaktorjeZehnTausendEuroVorsorgeguthaben = null;
+    this.formModel.garantierterRentenfaktorjeZehnTausendEuroVorsorgeguthaben = null;
+    this.formModel.aktuellejährlichgezahlteZulagen = null;
+    this.formModel.zulagen = {grundzulagen: false,
+      kinderzulagen: { anzahlderKinderdieberücksichtigtwerden:null}
+    };
+    this.formModel.kinder= {anzahlderKinder: null, geburtsDatumDerKinder: null};
+
   }
   ngOnInit() {
 
   }
 
   submitForm() {
-    var formData: any = new FormData();
 
-    formData.append("artderVersicherung", this.form.get('artderVersicherung').value);
-    formData.append("datumderStandmitteilung", this.form.get('datumderStandmitteilung').value);
-    formData.append("versicherungsbeginn", this.form.get('versicherungsbeginn').value);
-    formData.append("datumRentenbeginn", this.form.get('datumRentenbeginn').value);
-    formData.append("aktuelleBeitragszahlungen", this.form.get('aktuelleBeitragszahlungen').value);
-    formData.append("aktuellesVorsorgevermögen", this.form.get('aktuellesVorsorgevermögen').value);
-    formData.append("garantiezins", this.form.get('garantiezins').value);
-    formData.append("ausgewiesenederzeitigenÜberschussbeteiligung", this.form.get('ausgewiesenederzeitigenÜberschussbeteiligung').value);
-    formData.append("derzeitigeGesamtverzinsung", this.form.get('derzeitigeGesamtverzinsung').value);
-    formData.append("höhedergarantiertenAltersrente", this.form.get('höhedergarantiertenAltersrente').value);
-    formData.append("höhederAltersrentebeiderzeitigenÜberschussbeteiligungen", this.form.get('höhederAltersrentebeiderzeitigenÜberschussbeteiligungen').value);
-    formData.append("höhedergarantiertenAltersrentebeiFortzahlungderderzeitigenBeiträge", this.form.get('höhedergarantiertenAltersrentebeiFortzahlungderderzeitigenBeiträge').value);
-    formData.append("fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginn", this.form.get('fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginn').value);
-    formData.append("fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginnbeiFortzahlungderderzeitigenBeiträge", this.form.get('fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginnbeiFortzahlungderderzeitigenBeiträge').value);
-    formData.append("hochrechnungdesgarantiertenAltersvorsorgevermögens", this.form.get('hochrechnungdesgarantiertenAltersvorsorgevermögens').value);
-    formData.append("hochrechnungdesAltersvorsorgevermögens", this.form.get('hochrechnungdesAltersvorsorgevermögens').value);
-    formData.append("rentenfaktor", this.form.get('rentenfaktor').value);
-    formData.append("aktuellerRentenfaktorjeZehnTausendEuroVorsorgeguthaben", this.form.get('aktuellerRentenfaktorjeZehnTausendEuroVorsorgeguthaben').value);
-    formData.append("garantierterRentenfaktorjeZehnTausendEuroVorsorgeguthaben", this.form.get('hochrechnungdesAltergarantierterRentenfaktorjeZehnTausendEuroVorsorgeguthabensvorsorgevermögens').value);
-    formData.append("aktuellejährlichgezahlteZulagen", this.form.get('aktuellejährlichgezahlteZulagen').value);
-    formData.append("zulagen",(
-      formData.append("grundzulagen", this.form.get('grundzulagen').value),
-      formData.append("kinderzulagen", (formData.append("anzahlderKinderdieberücksichtigtwerden", this.form.get('anzahlderKinderdieberücksichtigtwerden').value)))
-      )
-    );
-    formData.append("kinder",(
-    formData.append("anzahlderKinder", this.form.get('anzahlderKinder').value),
-    formData.append("geburtsDatumDerKinder",this.form.get('geburtsDatumDerKinder').value)));
-    for (let value of formData.entries()) {
-    console.log(value[0] + ": " + value[1]);
-    }
-    return formData;
+    this.formModel.artderVersicherung = this.form.get('artderVersicherung').value;
+    this.formModel.datumderStandmitteilung =this.form.get('datumderStandmitteilung').value;
+    this.formModel.datumRentenbeginn = this.form.get('datumRentenbeginn').value;
+    this.formModel.aktuelleBeitragszahlungen =this.form.get('aktuelleBeitragszahlungen').value;
+    this.formModel.aktuellesVorsorgevermögen =this.form.get('aktuellesVorsorgevermögen').value;
+    this.formModel.garantiezins =this.form.get('garantiezins').value;
+    this.formModel.ausgewiesenederzeitigenÜberschussbeteiligung =this.form.get('ausgewiesenederzeitigenÜberschussbeteiligung').value;
+    this.formModel.derzeitigeGesamtverzinsung =this.form.get('derzeitigeGesamtverzinsung').value;
+    this.formModel.höhedergarantiertenAltersrente =this.form.get('höhedergarantiertenAltersrente').value;
+    this.formModel.höhederAltersrentebeiderzeitigenÜberschussbeteiligungen =this.form.get('höhederAltersrentebeiderzeitigenÜberschussbeteiligungen').value;
+    this.formModel.höhedergarantiertenAltersrentebeiFortzahlungderderzeitigenBeiträge =this.form.get('höhedergarantiertenAltersrentebeiFortzahlungderderzeitigenBeiträge').value;
+    this.formModel.fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginn =this.form.get('fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginn').value;
+    this.formModel.fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginnbeiFortzahlungderderzeitigenBeiträge =this.form.get('fondskapitalbeiFondswachstumvonXProzentbeiRentenbeginnbeiFortzahlungderderzeitigenBeiträge').value;
+    this.formModel.hochrechnungdesgarantiertenAltersvorsorgevermögens =this.form.get('hochrechnungdesgarantiertenAltersvorsorgevermögens').value;
+    this.formModel.hochrechnungdesAltersvorsorgevermögens =this.form.get('hochrechnungdesAltersvorsorgevermögens').value;
+    this.formModel.rentenfaktor =this.form.get('rentenfaktor').value;
+    this.formModel.aktuellerRentenfaktorjeZehnTausendEuroVorsorgeguthaben =this.form.get('aktuellerRentenfaktorjeZehnTausendEuroVorsorgeguthaben').value;
+    this.formModel.garantierterRentenfaktorjeZehnTausendEuroVorsorgeguthaben =this.form.get('garantierterRentenfaktorjeZehnTausendEuroVorsorgeguthaben').value;
+    this.formModel.aktuellejährlichgezahlteZulagen =this.form.get('aktuellejährlichgezahlteZulagen').value;
+    this.formModel.zulagen.grundzulagen =  this.form.get('grundzulagen').value;
+    this.formModel.zulagen.kinderzulagen.anzahlderKinderdieberücksichtigtwerden =  this.form.get('anzahlderKinderdieberücksichtigtwerden').value;
+    this.formModel.kinder.anzahlderKinder =  this.form.get('anzahlderKinder').value;
+    this.formModel.kinder.geburtsDatumDerKinder = this.form.get('geburtsDatumDerKinder').value;
+    this.formData.append("model", JSON.stringify(this.formModel));
+    console.log(this.formData.getAll("model"));
+/*
+    for (let [key, value] of this.formData.entries()) {
+      console.log(`${key}: ${value}`);
+    }*/
+    console.log(this.formModel);
+
+    return this.formData;
   }
 }
